64e00151698b6c55a8112fbc7b6229f07cf41f37
Lucas Machado <lucasmachado.rj@gmail.com>
2023-06-27 00:37:43 -0300

    [green] adding one todo to an existing list

--- src/pages/todo/todo-controller.spec.ts
+++ src/pages/todo/todo-controller.spec.ts
@@ -61,5 +61,52 @@ describe("TodoController", () => {
       expect(todos[0].getId()).toBeDefined();
       expect(todos[0].getCompleted()).toBeFalsy();
     });
+
+    test("adding one todo to an existing list", async () => {
+      const existingTodo = {
+        id: "1",
+        text: "Existing Todo",
+        completed: true,
+      };
+
+      const createTodoMock = new CreateTodoMockBuilder()
+        .withVariables({
+          text: todoText,
+        })
+        .withResponse({
+          id: "2",
+          text: todoText,
+          completed: false,
+        })
+        .build();
+
+      const getAllTodosStub = new GetAllTodosMockBuilder()
+        .withResponse([
+          existingTodo,
+          { id: "2", text: todoText, completed: false },
+        ])
+        .build();
+
+      client = new ApolloClientBuilder().withMocks([
+        createTodoMock,
+        getAllTodosStub,
+      ]);
+
+      todoRepository = new TodoRepository(client);
+      todoPresenter = new TodoPresenter(client);
+      const todoController = new TodoController(todoRepository);
+
+      await todoController.createTodo(todoText);
+      let todos = await todoPresenter.getAllTodos();
+
+      expect(todos).toHaveLength(2);
+      expect(todos[0].getText()).toEqual(existingTodo.text);
+      expect(todos[0].getId()).toEqual(existingTodo.id);
+      expect(todos[0].getCompleted()).toEqual(existingTodo.completed);
+
+      expect(todos[1].getText()).toEqual(todoText);
+      expect(todos[1].getId()).toBeDefined();
+      expect(todos[1].getCompleted()).toBeFalsy();
+    });
   });
 });
